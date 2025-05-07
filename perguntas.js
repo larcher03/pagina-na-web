b25b2e773d3df41a1465f0eb936a6ecf07a4836a
Amanda Larcher <cavalcanti.amanda@escola.pr.gov.br>
2025-05-07 09:32:56 -0300

    07 de maio

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Dorama',
+    'Com quem a Lim Joo Kyung namora no final',
+    'Ela namora com o Lee Su-ho'
+)
+
+criaCartao(
+    'Geografia',
+    'A capital da Coreia do sul é?',
+    'A capital da Coreia do sul é'
+)
+
+criaCartao(
+    'Kpop',
+    'Quem é o lider do stray kids?',
+    'O lider é o Bangchan'
+)
+
+criaCartao(
+    'Dorama',
+    'Qual dorama tem como um demônio como personagem principal?',
+    'o dorama é Meu demônio favorito'
+)
