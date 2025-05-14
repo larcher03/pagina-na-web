4a8dc0668f4037855e3eb1b7c4ccd1dd9b8c456c
Amanda Larcher <cavalcanti.amanda@escola.pr.gov.br>
2025-05-14 09:07:29 -0300

    14.04

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Dorama',
-    'Com quem a Lim Joo Kyung namora no final',
+    'Com quem a Lim Joo Kyung namora no final?',
     'Ela namora com o Lee Su-ho'
 )
 
 criaCartao(
     'Geografia',
     'A capital da Coreia do sul é?',
-    'A capital da Coreia do sul é'
+    'A capital da Coreia do sul é Seul'
 )
 
 criaCartao(
-    'Kpop',
-    'Quem é o lider do stray kids?',
-    'O lider é o Bangchan'
+    'Inglês',
+    'Como se fala "Olá" em inglês?',
+    'Se fala "Hello"'
 )
 
 criaCartao(
     'Dorama',
     'Qual dorama tem como um demônio como personagem principal?',
-    'o dorama é Meu demônio favorito'
+    'O dorama se chama Meu Demonio Favorito'
 )
